eb441a99fe7e0b1af6c5592b6875761d5c0a7e8b
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-07-18 14:16:31 -0300

    inserindo padrões de estilos em app.css e iniciando manipulação de links no componente header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,29 @@
+import Logo from "../../assets/img/logo.svg";
+import { links } from "../../Data";
+import Link from "../links/Link";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <nav className="nav container">
+        <a href="/" className="nav-logo">
+          <img src={Logo} alt="" className="nav-logo-img" />
+        </a>
+
+        <div className="nav-menu">
+          <ul className="nav-list">
+            {links.map((link, index) => {
+              return(
+                <li className="nav-item" key={index}>
+                  <Link />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 

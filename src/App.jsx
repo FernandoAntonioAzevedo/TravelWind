58512aff56212f417f3f2232e3335c3cd0df4b77
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-07-18 03:01:20 -0300

    avancando em estruturação de projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import "./App.css"
 
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Categories from "./components/categories/Categories"
+import About from "./components/about/About"
+import Destinations from "./components/destinations/Destinations"
+import Tours from "./components/tours/Tours"
+import Footer from "./components/footer/Footer"
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Home />
+      <Categories />
+      <About />
+      <Destinations />
+      <Tours />
+      <Footer />
+    </>
   )
 }
 

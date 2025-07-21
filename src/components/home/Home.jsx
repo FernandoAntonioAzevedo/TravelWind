a617d6a3a1a7a6abe08ca1c91ea6d68723591b5d
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-07-20 23:56:08 -0300

    avancando em home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,38 @@
+import { PiGlobeHemisphereEastFill } from "react-icons/pi";
+import ScrollLink from "../links/ScrollLink";
+import { RiTelegram2Line } from "react-icons/ri";
+
 
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home section">
+      <div className="home-container container grid">
+        <div className="home-data">
+          <h3 className="home-subtitle">
+            <PiGlobeHemisphereEastFill />
+            Let's Start your Journey
+          </h3>
+          <h1 className="home-title">
+            Your Journey Begins Here - Find, Book, Travel!
+          </h1>
 
-export default Home
+          <p className="home-description">
+            Discover, plan, and book unforgetable journeys with our expert
+            recommendations and travel inspiration.
+          </p>
+
+        <ScrollLink 
+            to="about" 
+            name="Sobre nós" 
+            className="button nav-button" 
+          >
+            <RiTelegram2Line className="button-icon" />
+          </ScrollLink>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
